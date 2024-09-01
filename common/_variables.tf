00da5142fb6a5a0fb725bed44f04c8c71bcddeed
Takehiro Tanaka <t.soccer.0916@gmail.com>
2024-09-01 13:30:36 +0900

    ワークフローを修正

--- common/_variables.tf
+++ common/_variables.tf
@@ -152,7 +152,7 @@ variable "half_maintenance" {
 variable "waf_region_count" {
   description = "trueにすると国別アクセスをCountするためのWAFが作成される"
   type        = bool
-  default     = false
+  default     = true
 }
 
 variable "waf_rule_regional_limit" {
