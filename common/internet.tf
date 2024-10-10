0b029d31e0ceb646666ca39d289062ec4b50f30b
takehiro1111 <t.soccer.0916@gmail.com>
2024-10-11 00:49:06 +0900

    レコードの設定&ACM及びvalidationの設定

--- common/internet.tf
+++ common/internet.tf
@@ -97,7 +97,7 @@ module "route53_records_takehiro1111_com" {
   version = "4.1.0"
 
   create    = true
-  zone_name = module.value.takehiro1111_com
+  zone_id = module.route53_zones.route53_zone_zone_id.takehiro1111_com
 
   records_jsonencoded = jsonencode([
     {
@@ -149,6 +149,49 @@ resource "aws_acm_certificate_validation" "tanaka_cloud_net_us_east_1" {
   validation_record_fqdns = [for record in aws_route53_record.tanaka_cloud_net_us_east_1 : record.fqdn]
   provider                = aws.us-east-1
 }
+
+## us-east-1
+module "acm_takehiro1111_com_us_east_1" {
+  source  = "terraform-aws-modules/acm/aws"
+  version = "5.1.0"
+
+  # aws_acm_certificate
+  create_certificate = true
+  domain_name = module.value.wildcard_takehiro1111_com
+  subject_alternative_names = [module.value.takehiro1111_com]
+  validation_method  = "DNS"
+
+  # aws_route53_record
+  create_route53_records = true
+  zone_id = module.route53_zones.route53_zone_zone_id.takehiro1111_com
+
+  # aws_acm_certificate_validation
+  wait_for_validation = true
+
+  providers = {
+    aws = aws.us-east-1
+  }
+}
+
+## ap-northeast-1
+module "acm_takehiro1111_com_ap_northeast_1" {
+  source  = "terraform-aws-modules/acm/aws"
+  version = "5.1.0"
+
+  # aws_acm_certificate
+  create_certificate = true
+  domain_name = module.value.wildcard_takehiro1111_com
+  subject_alternative_names = [module.value.takehiro1111_com]
+  validation_method  = "DNS"
+
+  # aws_route53_record
+  create_route53_records = true
+  zone_id = module.route53_zones.route53_zone_zone_id.takehiro1111_com
+
+  # aws_acm_certificate_validation
+  wait_for_validation = false
+}
+
 
 #####################################################
 # CloudFront
