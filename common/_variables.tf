d5c63f198bb904cd222c9a882cd506851a2b9882
Takehiro Tanaka <t.soccer.0916@gmail.com>
2024-10-11 17:43:36 +0900

    WAFの設定削除

--- common/_variables.tf
+++ common/_variables.tf
@@ -152,13 +152,13 @@ variable "half_maintenance" {
 variable "waf_region_count" {
   description = "trueにすると国別アクセスをCountするためのWAFが作成される"
   type        = bool
-  default     = true
+  default     = false
 }
 
 variable "waf_rule_regional_limit" {
   description = "falseにすると地域制限のルールが無効化になる。"
   type        = bool
-  default     = true
+  default     = false
 }
 
 variable "waf_regional_limit" {
