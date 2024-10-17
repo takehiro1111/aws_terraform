3aabb48d04d5acddec646547e7c8a0c798480e8e
takehiro1111 <t.soccer.0916@gmail.com>
2024-10-17 18:57:27 +0900

    リファクタリング

--- common/monitor.tf
+++ common/monitor.tf
@@ -7,17 +7,6 @@ resource "aws_cloudwatch_log_group" "ecs_nginx" {
   name              = "/ecslogs/nginx"
 }
 
-resource "aws_cloudwatch_log_group" "stg_2" {
-  retention_in_days = 7
-  name              = "/ecslogs/stg-2"
-  kms_key_id        = aws_kms_key.cloudwatch_logs.arn
-}
-
-resource "aws_cloudwatch_log_group" "ecs_event" {
-  retention_in_days = 0
-  name              = "/ecs_event/nginx"
-}
-
 # EC2 ------------------------------------------
 resource "aws_cloudwatch_log_group" "public_instance" {
   name              = "/compute/ec2/public"
@@ -29,7 +18,7 @@ resource "aws_cloudwatch_log_group" "public_instance" {
 # VPCフローログ --------------------------------
 resource "aws_cloudwatch_log_group" "flow_log" {
   name              = "/vpc/flow-log"
-  log_group_class   = "STANDARD" // https://aws.amazon.com/jp/blogs/news/new-amazon-cloudwatch-log-class-for-infrequent-access-logs-at-a-reduced-price/
+  log_group_class   = "STANDARD"
   skip_destroy      = true
   retention_in_days = 7
 }
@@ -41,46 +30,11 @@ resource "aws_cloudwatch_log_group" "flow_log" {
 # }
 
 #####################################################
-# Cloudwatch Alerm
-#####################################################
-
-#####################################################
-# Parameter Store
-#####################################################
-# Aurora ---------------------------------
-resource "aws_ssm_parameter" "aurora_mysql" {
-  name        = "/aurora/MYSQL_PASSWORD"
-  description = "Aurora MYSQL Master Password"
-  type        = "SecureString"
-  value       = "画面上から対応"
-
-  lifecycle {
-    ignore_changes = [value]
-  }
-}
-
-# Lambda ---------------------------------
-resource "aws_ssm_parameter" "slack_webhook_url" {
-  name        = "/${local.servicename}/SLACK_WEBHOOK"
-  description = "Slack Webhook lambda_notify channel"
-  type        = "SecureString"
-  value       = "画面上から対応"
-
-  lifecycle {
-    ignore_changes = [value]
-  }
-}
-#####################################################
 # EventBridge
 #####################################################
-# variable "ecs_cluster_arn" {
-#   description = "ECS Cluster ARN"
-#   type = list(string)
-#   default = [
-#     aws_ecs_cluster.web.arn
-#   ]
-# }
-
+/* 
+ * ECS Task STOPPED Event 
+ */
 resource "aws_cloudwatch_event_rule" "ecs_event" {
   state       = "ENABLED"
   name        = "ecs-event-notify"
@@ -112,17 +66,11 @@ resource "aws_cloudwatch_event_target" "ecs_event" {
 
   input_transformer {
     input_paths = {
-      "account": "$.account",
-      "availabilityZone": "$.detail.availabilityZone",
-      "clusterArn": "$.detail.clusterArn",
       "group": "$.detail.group",
       "taskDefinitionArn": "$.detail.taskDefinitionArn",
       "stoppedAt": "$.detail.stoppedAt",
       "stopCode": "$.detail.stopCode",
       "stoppedReason": "$.detail.stoppedReason",
-      "containerExitCode": "$.detail.containers[0].exitCode",
-      "containerReason": "$.detail.containers[0].reason",
-      
     }
     input_template = <<END
     {
@@ -131,39 +79,8 @@ resource "aws_cloudwatch_event_target" "ecs_event" {
       "content": {
         "textType": "client-markdown",
         "title": ":warning: ECSタスクが停止されました :warning:",
-        "description": "overview\n ・Service:`<group>`\n・Task: `<taskDefinitionArn>`\n・stoppedAt: `<stoppedAt>(UTC)`\n・stopCode: `<stopCode>`\n・stoppedReason: `<stoppedReason>`\n・containerExitCode:`<containerExitCode>`\n・containerReason:`<containerReason>`"
+        "description": "overview\n ・Service:`<group>`\n・Task: `<taskDefinitionArn>`\n・stoppedAt: `<stoppedAt>(UTC)`\n・stopCode: `<stopCode>`\n・stoppedReason: `<stoppedReason>`"
       }
-    }
-    END
-  }
-}
-resource "aws_cloudwatch_event_target" "ecs_event_2" {
-  target_id = "ecs-event-notify-cwlogs"
-  rule      = aws_cloudwatch_event_rule.ecs_event.name
-  arn       = aws_cloudwatch_log_group.ecs_event.arn
-
-  input_transformer {
-    input_paths = {
-      "account": "$.account",
-      "availabilityZone": "$.detail.availabilityZone",
-      "clusterArn": "$.detail.clusterArn",
-      "group": "$.detail.group",
-      "taskDefinitionArn": "$.detail.taskDefinitionArn",
-      "stoppedAt": "$.detail.stoppedAt",
-      "stopCode": "$.detail.stopCode",
-      "stoppedReason": "$.detail.stoppedReason",
-      "containerExitCode": "$.detail.containers[0].exitCode",
-      "containerReason": "$.detail.containers[0].reason"
-    }
-    input_template = <<END
-    {
-      "Service": "<group>",
-      "Task": "<taskDefinitionArn>",
-      "StoppedAt": "<stoppedAt>",
-      "StopCode": "<stopCode>",
-      "StoppedReason": "<stoppedReason>",
-      "ContainerExitCode": "<containerExitCode>",
-      "ContainerReason": "<containerReason>"
     }
     END
   }
@@ -193,60 +110,6 @@ resource "aws_cloudwatch_event_target" "ecs_event_2" {
 #   target_id = "update_waf_rule_lambda"
 #   arn       = aws_lambda_function.update_waf_rule.arn
 # }
-
-
-#####################################################
-# Athena
-#####################################################
-# resource "aws_athena_workgroup" "test" {
-#   name = aws_s3_bucket.athena.id
-
-#   configuration {
-#     enforce_workgroup_configuration    = true
-#     publish_cloudwatch_metrics_enabled = false
-#     result_configuration {
-#       output_location = "s3://${aws_s3_bucket.athena.id}/athena-result/"
-#       encryption_configuration {
-#         encryption_option = "SSE_S3"
-#       }
-#     }
-#   }
-# }
-
-# resource "aws_athena_database" "test" {
-#   name   = "test_employee_list"
-#   bucket = aws_s3_bucket.athena.id
-# }
-
-# data "template_file" "flow_log" {
-#   template = file("${path.module}/sql/create-table.sql.tpl")
-#   vars = {
-#     athena_database_name = var.athena_database_name
-#     athena_table_name    = var.athena_table_name
-#     log_bucket_name      = var.log_bucket_name
-#   }
-# }
-
-# resource "aws_athena_named_query" "flow_log" {
-#   name        = "Create table"
-#   description = "テーブルを作成"
-#   workgroup   = aws_athena_workgroup.flow_log.id
-#   database    = aws_athena_database.flow_log.id
-#   query       = data.template_file.flow_log.rendered
-# }
-
-#####################################################
-# SNS
-#####################################################
-resource "aws_sns_topic" "lambda_mail" {
-  name = "lambda-mail-sns-topic"
-}
-
-resource "aws_sns_topic_subscription" "lambda_mail" {
-  topic_arn = aws_sns_topic.lambda_mail.arn
-  protocol  = "email"
-  endpoint  = module.value.my_gmail_address
-}
 
 ######################################################################
 # Config
@@ -308,38 +171,6 @@ data "aws_iam_policy_document" "sns_notify_chatbot" {
       "arn:aws:sns:ap-northeast-1:${data.aws_caller_identity.current.account_id}:slack_notify"
     ]
   }
-}
-
-######################################################################
-# SSM ParameterStore
-######################################################################
-locals {
-  slack_info = {
-    notify_slack_workspace = {
-      name        = "/slack/workspace_id/personal"
-      description = "個人用のSlackワークスペースID"
-    }
-    notify_slack_channel = {
-      name        = "/slack/channel_id/aws_alert"
-      description = "通知用のSlackチャンネルID"
-    }
-  }
-}
-
-resource "aws_ssm_parameter" "slack_info" {
-  for_each    = { for k, v in local.slack_info : k => v }
-  name        = each.value.name
-  description = each.value.description
-  type        = "SecureString"
-  value       = "コンソール画面で設定する。"
-
-  lifecycle {
-    ignore_changes = [value]
-  }
-}
-
-data "aws_ssm_parameter" "notify_slack_channel" {
-  name = "/slack/channel_id/aws_alert"
 }
 
 ######################################################################
@@ -480,7 +311,9 @@ locals {
 #   domain_name = "firehose-os-test"
 # }
 
-
+#####################################################
+# Athena
+#####################################################
 /**
  * VPC FlowLogs
  */
@@ -539,4 +372,60 @@ resource "aws_glue_crawler" "vpc_flow_logs" {
       }
     }
   EOF
+}
+
+######################################################################
+# SSM ParameterStore
+######################################################################
+locals {
+  slack_info = {
+    notify_slack_workspace = {
+      name        = "/slack/workspace_id/personal"
+      description = "個人用のSlackワークスペースID"
+    }
+    notify_slack_channel = {
+      name        = "/slack/channel_id/aws_alert"
+      description = "通知用のSlackチャンネルID"
+    }
+  }
+}
+
+resource "aws_ssm_parameter" "slack_info" {
+  for_each    = { for k, v in local.slack_info : k => v }
+  name        = each.value.name
+  description = each.value.description
+  type        = "SecureString"
+  value       = "コンソール画面で設定する。"
+
+  lifecycle {
+    ignore_changes = [value]
+  }
+}
+
+data "aws_ssm_parameter" "notify_slack_channel" {
+  name = "/slack/channel_id/aws_alert"
+}
+
+# Aurora ---------------------------------
+resource "aws_ssm_parameter" "aurora_mysql" {
+  name        = "/aurora/MYSQL_PASSWORD"
+  description = "Aurora MYSQL Master Password"
+  type        = "SecureString"
+  value       = "画面上から対応"
+
+  lifecycle {
+    ignore_changes = [value]
+  }
+}
+
+# Lambda ---------------------------------
+resource "aws_ssm_parameter" "slack_webhook_url" {
+  name        = "/${local.servicename}/SLACK_WEBHOOK"
+  description = "Slack Webhook lambda_notify channel"
+  type        = "SecureString"
+  value       = "画面上から対応"
+
+  lifecycle {
+    ignore_changes = [value]
+  }
 }
