c84015b06134cb8f953f6e007216bb5c34b2b944
takehiro1111 <t.soccer.0916@gmail.com>
2024-10-27 14:15:09 +0900

    ECSのポリシーを外付けのファイルではなくインラインで定義

--- common/storage.tf
+++ common/storage.tf
@@ -16,20 +16,62 @@ resource "aws_ecr_repository" "common" {
 
 resource "aws_ecr_repository_policy" "common" {
   for_each = toset(var.repo_list)
-
   repository = each.value
-  policy     = file("../policy/ecr_repo_policy.json")
-
   depends_on = [aws_ecr_repository.common]
+  policy     = <<END
+    {
+      "Version": "2008-10-17",
+      "Statement": [
+        {
+          "Sid": "AllowALL",
+          "Effect": "Allow",
+          "Principal": {
+            "AWS": "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+          },
+          "Action": [
+            "ecr:*"
+          ]
+        }
+      ]
+    }
+  END
 }
 
 resource "aws_ecr_lifecycle_policy" "common" {
   for_each = toset(var.repo_list)
-
   repository = each.value
-  policy     = file("../policy/ecr_lifecycle_policy.json")
-
   depends_on = [aws_ecr_repository.common]
+  policy     = <<END
+    {
+      "rules": [
+        {
+          "rulePriority": 1,
+          "description": "Expire images older than 180 days",
+          "selection": {
+            "tagStatus": "untagged",
+            "countType": "sinceImagePushed",
+            "countUnit": "days",
+            "countNumber": 180
+          },
+          "action": {
+            "type": "expire"
+          }
+        },
+        {
+          "rulePriority": 2,
+          "description": "Keep last 10 images",
+          "selection": {
+            "tagStatus": "any",
+            "countType": "imageCountMoreThan",
+            "countNumber": 10
+          },
+          "action": {
+            "type": "expire"
+          }
+        }
+      ]
+    }
+  END
 }
 
 #####################################################
