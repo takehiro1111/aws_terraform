a55fbb94020ced19e024fc522b2525d399c04644
takehiro1111 <t.soccer.0916@gmail.com>
2024-10-28 23:40:40 +0900

    Organizationsのコード作成

---
+++ common/account_management/_local.tf
@@ -0,0 +1,47 @@
+locals {
+  service_name = "account_management"
+  repo = "aws_terraform"
+  dir  = "common/account_management"
+}
+
+/* 
+ * Organizations Parameter 
+ */
+locals {
+  members = {
+    member_1 = {
+      name = substr(module.value.my_gmail_alias_address.member_1,14,14)
+      email = module.value.my_gmail_alias_address.member_1
+      parent_id = aws_organizations_organizational_unit.ou["ou_1"].id
+    }
+  }
+
+  ou = {
+    ou_1 = {
+      name = trimsuffix(local.dir,"/account_management")
+      parent_id = aws_organizations_organization.org.roots[0].id
+    }
+  }
+
+  aws_service_access_principals = [
+    "access-analyzer.amazonaws.com",
+    "health.amazonaws.com",
+    "reporting.trustedadvisor.amazonaws.com",
+    "sso.amazonaws.com",
+    "cloudtrail.amazonaws.com",
+    "inspector2.amazonaws.com",
+    "config.amazonaws.com",
+    "detective.amazonaws.com",
+    "securityhub.amazonaws.com",
+    "ram.amazonaws.com",
+    "wellarchitected.amazonaws.com",
+    "member.org.stacksets.cloudformation.amazonaws.com",
+    "storage-lens.s3.amazonaws.com",
+    "reachabilityanalyzer.networkinsights.amazonaws.com",
+    "cost-optimization-hub.bcm.amazonaws.com",
+    "compute-optimizer.amazonaws.com",
+    "guardduty.amazonaws.com",
+    "malware-protection.guardduty.amazonaws.com",
+  ]
+
+}
---
+++ common/account_management/config.tf
@@ -0,0 +1,76 @@
+#####################################################
+# Terraform Block
+#####################################################
+terraform {
+  required_version = "1.9.8"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.73.0"
+    }
+  }
+
+  backend "s3" {
+    bucket  = "tfstate-685339645368"
+    key     = "account_management/tfstate"
+    region  = "ap-northeast-1"
+    profile = "my_account"
+  }
+}
+
+#####################################################
+# Provider Block
+#####################################################
+provider "aws" {
+  region = "ap-northeast-1"
+  profile = "my_account"
+
+  default_tags {
+    tags = {
+      Name        = local.service_name
+      repository  = local.repo
+      directory   = local.dir
+    }
+  }
+}
+
+provider "aws" {
+  alias  = "us-east-1"
+  region = "us-east-1"
+  profile = "my_account"
+
+  default_tags {
+    tags = {
+      Repository = local.repo
+      Directory  = local.dir
+    }
+  }
+}
+
+#####################################################
+# PMOdule Block
+#####################################################
+module "value" {
+  source = "../../modules/variable"
+}
+
+#####################################################
+# Data Block
+#####################################################
+data "aws_caller_identity" "self" {}
+
+data "aws_partition" "current" {}
+
+data "aws_region" "default" {
+  name = "ap-northeast-1"
+}
+
+# data "terraform_remote_state" "state_personal_account" {
+#   backend = "s3"
+#   config = {
+#     bucket         = "tfstate-685339645368"
+#     key            = "state/state_prod"
+#     region         = data.aws_region.default.name
+#   }
+# }
