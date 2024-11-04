cbbb928778ef19e9536ba442c01a2f864593b067
takehiro1111 <t.soccer.0916@gmail.com>
2024-11-04 09:40:01 +0900

    不要なコードの削除

--- development/prod/internet.tf
+++ development/prod/internet.tf
@@ -306,45 +306,6 @@ module "cloudfront_api_takehiro1111_com" {
   }
 }
 
-# resource "aws_cloudfront_origin_request_policy" "header_whitelist_authorization" {
-#   name    = "example-policy"
-#   comment = "example comment"
-#   cookies_config {
-#     cookie_behavior = "none"
-#   }
-#   headers_config {
-#     header_behavior = "whitelist"
-#     headers {
-#       items = ["Authorization"]
-#     }
-#   }
-#   query_strings_config {
-#     query_string_behavior = "none"
-#   }
-# }
-
-# resource "aws_cloudfront_cache_policy" "header_whitelist_authorization" {
-#   name        = "example-policy"
-#   comment     = "test comment"
-#   default_ttl = 0
-#   max_ttl     = 0
-#   min_ttl     = 0
-#   parameters_in_cache_key_and_forwarded_to_origin {
-#     cookies_config {
-#       cookie_behavior = "none"
-#     }
-#     headers_config {
-#       header_behavior = "whitelist"
-#       headers {
-#         items = ["Authorization"]
-#       }
-#     }
-#     query_strings_config {
-#       query_string_behavior = "none"
-#     }
-#   }
-# }
-
 #####################################################
 # ALB
 #####################################################
