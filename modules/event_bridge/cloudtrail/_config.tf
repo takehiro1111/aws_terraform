b8e087751f2a5a50bff6bd808645788fdf5fc0c0
takehiro1111 <t.soccer.0916@gmail.com>
2024-11-17 15:42:01 +0900

    pre-commitの編集

--- modules/event_bridge/cloudtrail/_config.tf
+++ modules/event_bridge/cloudtrail/_config.tf
@@ -1,5 +1,16 @@
+terraform {
+  required_version = ">=1.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">=5.76.0"
+    }
+  }
+}
+
 provider "aws" {
-  region  = "ap-northeast-1"
+  region = "ap-northeast-1"
 
   default_tags {
     tags = {
