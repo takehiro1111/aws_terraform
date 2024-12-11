53c8d87560cb6e2c52ccb62f2a77db22829c1d98
takehiro1111 <t.soccer.0916@gmail.com>
2024-12-11 23:51:16 +0900

    センシティブな値をマスク

--- modules/variable/slack.tf
+++ modules/variable/slack.tf
@@ -1,10 +1,12 @@
 output "slack_workspace_id" {
   description = "個人用のSlackワークスペースID"
   value       = "T06PFGXUB2B" # personal 
+  sensitive   = true
 }
 
 output "aws_alert_slack_channel_id" {
   description = "AWS系の通知用のSlackチャンネルID"
   value       = "C07GTL63RDJ" # aws_alert
+  sensitive   = true
 }
 
