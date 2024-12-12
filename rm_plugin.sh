e34551fd94ba8c4707bd3a7cbe0141a9e3222551
takehiro1111 <t.soccer.0916@gmail.com>
2024-12-13 08:59:16 +0900

    pluginの容量削除

--- rm_plugin.sh
+++ rm_plugin.sh
@@ -2,7 +2,7 @@
 PROJECTS=($(find . -type f -name ".envrc"))
 MAIN_DIR=$(pwd)
 
-# 色の定義
+# # 色の定義
 GREEN='\033[0;32m'
 NC='\033[0m'
 
@@ -22,3 +22,26 @@ for project in "${PROJECTS[@]}" ; do
     fi
   popd > /dev/null
 done
+
+# ENVIRONMENTS=("compute", "production" "acc")
+# # 色の定義
+# GREEN='\033[0;32m'
+# NC='\033[0m' 
+
+# for project in "${PROJECTS[@]}" ; do
+#     # .envrcファイルのあるディレクトリに移動
+#     PROJECT_DIR=$(dirname "$project")
+#     # ディレクトリに移動してdirenv allowを実行し、元のディレクトリに戻る
+#     pushd "${MAIN_DIR}/${PROJECT_DIR}" > /dev/null
+#     for env in "${ENVIRONMENTS[@]}"; do
+#         if [ -d "${env}/.terraform" ]; then
+#             rm -rf "${env}/.terraform/"
+#             if [ $? -eq 0 ]; then
+#                 echo -e "${GREEN}✔${NC} Successfully removed .terraform directory in ${PROJECT_DIR}/${env}"
+#             fi
+#         else
+#             echo "No .terraform directory found in ${PROJECT_DIR}/${env}"
+#         fi
+#     done
+#     popd > /dev/null
+# done
