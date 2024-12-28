6a09cbfee1f459a322fabd50d8c0009ddeb88826
takehiro1111 <t.soccer.0916@gmail.com>
2024-12-28 23:09:09 +0900

    github用のstateを管理

---
+++ state/gh/_config.tf
@@ -0,0 +1,56 @@
+#####################################################
+# Terraform Block
+#####################################################
+terraform {
+  required_version = "1.10.3"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.82.2"
+    }
+  }
+
+  backend "s3" {
+    bucket  = "tfstate-685339645368"
+    key     = "state/state_gh"
+    region  = "ap-northeast-1"
+    profile = "master_administrator"
+  }
+}
+
+#####################################################
+# Provider Block
+#####################################################
+provider "aws" {
+  region  = "ap-northeast-1"
+  profile = "master_administrator"
+
+  default_tags {
+    tags = {
+      Repository = local.repo
+      Directory  = local.dir
+    }
+  }
+}
+
+#####################################################
+# PMOdule Block
+#####################################################
+module "value" {
+  source = "../../modules/variable"
+}
+
+#####################################################
+# Data Block
+#####################################################
+data "aws_caller_identity" "self" {}
+
+data "terraform_remote_state" "development_state" {
+  backend = "s3"
+  config = {
+    bucket = "tfstate-650251692423"
+    key    = "state/development"
+    region = "ap-northeast-1"
+  }
+}
---
+++ state/gh/s3.tf
@@ -0,0 +1,78 @@
+#####################################################
+# S3
+#####################################################
+/* 
+ * tfstate
+ */
+# ref: https://registry.terraform.io/modules/terraform-aws-modules/s3-bucket/aws/latest
+module "s3_bucket_gh_tfstate" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "4.3.0"
+
+  # aws_s3_bucket
+  bucket              = "tfstate-github-${data.aws_caller_identity.self.account_id}"
+  force_destroy       = false // オブジェクトが入っていても強制的に削除可能
+  object_lock_enabled = false
+
+  # aws_s3_bucket_ownership_controls
+  control_object_ownership = true
+  object_ownership         = "BucketOwnerPreferred"
+
+  # aws_s3_bucket_acl
+  acl = "private"
+
+  # aws_s3_bucket_versioning
+  versioning = {
+    enabled = true
+  }
+
+  # aws_s3_bucket_server_side_encryption_configuration
+  server_side_encryption_configuration = {
+    rule = {
+      bucket_key_enabled = false
+      apply_server_side_encryption_by_default = {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  # aws_s3_bucket_public_access_block
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+
+  # aws_s3_bucket_policy
+  attach_policy = true
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Principal" : {
+          AWS : [
+            "arn:aws:iam::${data.aws_caller_identity.self.account_id}:root",
+            "arn:aws:iam::${data.terraform_remote_state.development_state.outputs.account_id}:root"
+          ]
+        },
+        "Action" : [
+          "s3:GetBucketLocation",
+          "s3:ListBucket",
+          "s3:GetObject",
+          "s3:PutBucketAcl",
+          "s3:PutBucketPolicy",
+          "s3:PutObject",
+        ],
+        "Resource" : [
+          module.s3_bucket_gh_tfstate.s3_bucket_arn,
+          "${module.s3_bucket_gh_tfstate.s3_bucket_arn}/*"
+        ]
+        "Condition" : {
+          "StringEquals" : {
+            "aws:PrincipalOrgID" : module.value.org_id
+          }
+        }
+      }
+    ]
+  })
+}
